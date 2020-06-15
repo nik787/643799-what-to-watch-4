b5dfed6555aef811d5466ea22935c467817e5062
nik787 <nikito101d@gmail.com>
2020-06-15 16:09:05 +0300

    Добавит пропсы в компонент

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -2,9 +2,11 @@ import React from "react";
 import Main from "../main/main.jsx";
 
 
-const App = () => {
+const App = (props) => {
+  // eslint-disable-next-line react/prop-types
+  const {promoFilm} = props;
   return (
-    <Main />
+    <Main promoSettings={promoFilm} />
   );
 };
 
--- src/index.js
+++ src/index.js
@@ -2,7 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/app/app.jsx";
 
+const promoFilmSettings = {
+  TITLE: `The Grand Budapest Hotel`,
+  GENRE: `Drama`,
+  YEAR: 2014
+};
+
 ReactDOM.render(
-    <App />,
+    <App promoFilm={promoFilmSettings} />,
     document.querySelector(`#root`)
 );
