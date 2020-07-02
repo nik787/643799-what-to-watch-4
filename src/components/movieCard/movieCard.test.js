b866cb10bf25e14258af77ee61c2a45633a9ee64
nik787 <nikito101d@gmail.com>
2020-07-02 18:20:26 +0300

    Добавляет снепшот для карточки

--- src/components/movieCard/movieCard.test.js
+++ src/components/movieCard/movieCard.test.js
@@ -3,9 +3,10 @@ import renderer from "react-test-renderer";
 import MovieCard from "./movieCard.jsx";
 
 it(`Should MovieCard render correctly`, () => {
+  // const film = jest.fn().mockReturnValue(`Fantastic Beasts`);
   const tree = renderer
     .create(<MovieCard
-      title={[`Fantastic Beasts`, `Bohemian Rhapsody`, `Macbeth`]}
+      title={`Fantastic Beasts`}
       openFilm={() => {}} />)
     .toJSON();
 
