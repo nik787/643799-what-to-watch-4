2c798fc0f10425212e18bafa655e21b13c0d0122
nik787 <nikito101d@gmail.com>
2020-07-13 15:41:03 +0300

    Добавляет запуск видео по наведению и мьют

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,28 +1,51 @@
 import React, {PureComponent} from "react";
 import PropTypes from "prop-types";
 import {propTypesMovie} from "../../data-types/film.js";
+import VideoPlayer from "../video-player/video-player.jsx";
 
 
 class MovieCard extends PureComponent {
   constructor(props) {
     super(props);
 
-  }
-  render() {
-    const {movie, onTitleClick, onCardHover} = this.props;
-    const handleMovieClick = (evt) => {
-      evt.preventDefault();
-      onTitleClick(movie);
+    this.state = {
+      isPlaying: false,
     };
 
+    this._handleMovieClick = this._handleMovieClick.bind(this);
+  }
+
+  _handleMovieClick(evt) {
+    const onTitleClick = this.props.onTitleClick;
+
+    evt.preventDefault();
+    onTitleClick(this.props.movie);
+  }
+
+  render() {
+    const {movie, onCardHover} = this.props;
+
     return (
-      <article className="small-movie-card catalog__movies-card" onMouseOver={() => onCardHover(this.props.movie)}
+      <article className="small-movie-card catalog__movies-card"
+        onMouseEnter={() => {
+          this.setState({
+            isPlaying: true,
+          });
+          onCardHover();
+        }}
+        onMouseOut={() => {
+          this.setState({
+            isPlaying: false,
+          });
+        }}
       >
-        <div className="small-movie-card__image" onClick={handleMovieClick}>
-          <img src={movie.previewImage} alt={movie.name} width="280" height="175" />
+        <div className="small-movie-card__image" onClick={this._handleMovieClick}>
+          {movie.previewVideoLink
+            ? <VideoPlayer isPlaying={this.state.isPlaying} source={movie.previewVideoLink} poster={movie.previewImage} isMuted={true}/>
+            : <img src={movie.previewImage} alt={movie.name} width="280" height="175" />}
         </div>
         <h3 className="small-movie-card__title">
-          <a className="small-movie-card__link" href="movie-page.html" onClick={handleMovieClick}>{movie.name}</a>
+          <a className="small-movie-card__link" href="movie-page.html" onClick={this._handleMovieClick}>{movie.name}</a>
         </h3>
       </article>
     );
--- src/components/video-player/video-player.jsx
+++ src/components/video-player/video-player.jsx
@@ -1,19 +1,68 @@
-import React from 'react';
+import React, {PureComponent} from 'react';
 import PropTypes from 'prop-types';
+
+const player = {
+  WIDTH: 280,
+  HEIGHT: 175
+};
 
 const ERROR_MESSAGE = `Soory, your browser doesn't support video.`;
 
-const VideoPlayer = ({isPlaying, source, poster}) => {
-  return (
-    <video width="280" height="175" poster={poster} autoPlay={isPlaying ? `true` : ``}>
-      <source src={source}
-        type="video/webm" />
-      <source src={source}
-        type="video/mp4" />
-      {ERROR_MESSAGE}
-    </video>
-  );
-};
+export default class VideoPlayer extends PureComponent {
+  constructor(props) {
+    super(props);
+
+    this._videoRef = React.createRef();
+    this._playTimeout = null;
+    this.isPlaying = props.isPlaying;
+  }
+
+  componentDidMount() {
+    const {source, poster, isMuted} = this.props;
+    const video = this._videoRef.current;
+
+    video.src = source;
+    video.poster = poster;
+    video.muted = isMuted ? true : false;
+  }
+
+  componentWillUnmount() {
+    const video = this._videoRef.current;
+
+    video.src = ``;
+    video.poster = ``;
+    video.onplay = null;
+
+    clearTimeout(this._playTimeout);
+  }
+
+  render() {
+    return (
+      <video
+        width={player.WIDTH}
+        height={player.HEIGHT}
+        ref={this._videoRef}
+        onMouseOver={(evt) => evt.target.play()}
+        onMouseOut={(evt) => evt.target.pause()}
+      >
+        {ERROR_MESSAGE}
+      </video>
+    );
+  }
+  componentDidUpdate() {
+    const video = this._videoRef.current;
+
+    if (this.props.isPlaying) {
+      this._playTimeout = setTimeout(() => {
+        video.play();
+      }, 1000);
+    } else {
+      video.load();
+      clearTimeout(this._playTimeout);
+    }
+  }
+
+}
 
 VideoPlayer.propTypes = {
   isPlaying: PropTypes.bool.isRequired,
@@ -21,4 +70,3 @@ VideoPlayer.propTypes = {
   poster: PropTypes.string.isRequired,
 };
 
-export default VideoPlayer;
